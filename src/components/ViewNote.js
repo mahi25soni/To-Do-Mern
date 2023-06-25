23badea3881b828ac0be4a382346b4a35dace4ad
Mahendra Soni <msdraj02@protonmail.com>
2023-06-25 23:59:28 +0530

    something

--- src/components/ViewNote.js
+++ src/components/ViewNote.js
@@ -1,10 +1,19 @@
-import React from 'react'
-
+import React, {useContext} from 'react'
+import { NoteContext } from '../context/notes/NoteContext'
 export default function ViewNote() {
+    const notes = useContext(NoteContext)
+    // console.log(notes)
   return (
     <>
     <div className="container">
-        This is the container for viewing notes
+        <h2>Your Tasks</h2>
+        <div className="row">
+        {notes.map((element)=> {
+            <div className="col-3"> 
+            <p>;ksdjfh;ksdfhkj;sdh</p>
+            </div>
+        })}
+        </div>
     </div>
     </>
   )
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,14 +1,41 @@
 import {NoteContext}  from './NoteContext'
 import { useState } from 'react'
 const NoteState = (props) => {
-    const intial_notes = {
-        "name": 'mahendra Soni',
-        "class":"10th B"
-    }
+    const intial_notes = [
+            {
+                "_id": "64961e09c199b74ed90ad217",
+                "task_name": "add you second task",
+                "done": false,
+                "user_id": "6496131dad183e22d63f4b15",
+                "__v": 0
+            },
+            {
+                "_id": "64961e0dc199b74ed90ad21a",
+                "task_name": "add you third task",
+                "done": false,
+                "user_id": "6496131dad183e22d63f4b15",
+                "__v": 0
+            },
+            {
+                "_id": "64961e14c199b74ed90ad21d",
+                "task_name": "add you fourth task",
+                "done": false,
+                "user_id": "6496131dad183e22d63f4b15",
+                "__v": 0
+            },
+            {
+                "_id": "64961e18c199b74ed90ad220",
+                "task_name": "add you fifth task",
+                "done": false,
+                "user_id": "6496131dad183e22d63f4b15",
+                "__v": 0
+            }
+]
+    
 
 
     return (
-          <NoteContext.Provider value={{intial_notes}}>
+          <NoteContext.Provider value={intial_notes}>
             <div>{props.children}</div>
           </NoteContext.Provider>
       );
