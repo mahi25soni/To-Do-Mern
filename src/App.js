952a3d40c0d6d9b8d7022a3b28a11ad4c87595d3
Mahendra Soni <msdraj02@protonmail.com>
2023-06-25 23:03:10 +0530

    use Location added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import NoteState from "./context/notes/NoteState";
 
+
 function App() {
+
   return (
     <>
     <NoteState>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+
 
 export default function Navbar() {
+  const path = useLocation().pathname
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-secondary">
@@ -24,14 +27,14 @@ export default function Navbar() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
+                <a className= {`nav-link ${path==="/" ? "active":""}`} aria-current="page" href="/">
                   <strong>
                     Home
                   </strong>
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/about">
+                <a className={`nav-link ${path==="/about" ? "active":""}`} href="/about">
                   <strong>
                     About
                   </strong>
