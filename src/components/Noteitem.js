cb74919ad0ec3e8d7aafe735d8bafc108e863253
Mahendra Soni <msdraj02@protonmail.com>
2023-06-27 11:44:05 +0530

    note editing in process

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -3,10 +3,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faTrashCan, faPenToSquare} from '@fortawesome/free-regular-svg-icons'
 import '../index.css'
 import { NoteContext } from '../context/notes/NoteContext'
+
 export default function Noteitem(props) {
-  const {note} = props
+  const {note, editButton} = props
   const {deleteNote } = useContext(NoteContext)
 
+
+ 
 
   return (
     <>
@@ -16,7 +19,7 @@ export default function Noteitem(props) {
                 <p className="text-danger fw-bold text-decoration-underline d-inline">{note.title}</p>
                 <span style={{position : 'absolute', right: '10px'}}>
                     <FontAwesomeIcon icon={faTrashCan} onClick={()=> deleteNote(note._id)}  style={{marginLeft: "5px",cursor: "pointer"}}/>
-                    <FontAwesomeIcon icon={faPenToSquare} style={{marginLeft: "5px",     cursor: "pointer"}}/>
+                    <FontAwesomeIcon icon={faPenToSquare} onClick={() => editButton(note)} style={{marginLeft: "5px",     cursor: "pointer"}}/>
                 </span      >
                 <p className="text-primary mt-3">{note.description} </p>
             </div>
--- src/components/ViewNote.js
+++ src/components/ViewNote.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect} from 'react'
+import React, {useContext, useEffect, useRef, useState} from 'react'
 import { NoteContext } from '../context/notes/NoteContext'
 import Noteitem from './Noteitem'
 export default function ViewNote() {
@@ -8,14 +8,60 @@ export default function ViewNote() {
       // eslint-disable-next-line react-hooks/exhaustive-deps
     },[])
 
+    const updateNote = (note) => {
+      refUpdate.current.click()
+    }
+    const refUpdate = useRef(null)
+
+    const [notedata , setNotedata] = useState({id : "", title : "", description : ""})
+   
+    const inputOnChange = (event) => {
+      // console.log(notedata)
+      // console.log(event.target.value)
+      setNotedata({...note , [event.target.id] : event.target.value})
+      console.log(note)
+    }
+
   return (
     <>
     <div className="container mt-3">
+    <button type="button" ref={refUpdate} className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+        </button>
+
+        <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+            <div className="modal-content">
+            <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">Edit Note</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+            <div className="container  mt-3">
+                <div className="mb-3">
+                    <label htmlFor="title" className="form-label"> <strong>Note Title</strong></label>
+                    <input type="text" className="form-control" id="title" onChange={inputOnChange}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="description" className="form-label"><strong>Note Description</strong></label>
+                    <input type="text" className="form-control" id="description" onChange={inputOnChange}></input>
+                </div>
+            </div>
+            </div>
+            <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" className="btn btn-primary">Edit Note</button>
+            </div>
+            </div>
+        </div>
+        </div>
         <h2 className='text-center'>Your Tasks</h2>
         <div className="row">
+
+          
         {note.map((element, key)=> {
             return <div className="col col-sm-3 "> 
-            <Noteitem  key={element._id}  note = {element}></Noteitem>
+            <Noteitem  key={element._id}  note = {element} editButton={updateNote}></Noteitem>
             </div>
         })}
         </div>
